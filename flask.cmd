99a189e6602c464bc3a98d78881c1b038e38f041
Ubuntu <ubuntu@ip-172-31-23-185.ap-northeast-2.compute.internal>
2020-07-16 01:36:58 +0000

    0716_flask

---
+++ flask.cmd
@@ -0,0 +1,5 @@
+@echo off
+@cd /home/ubuntu/flask
+@export FLASK_APP=pybo
+@export FLASK_ENV=development
+
---
+++ pybo/views/main_views.py
@@ -0,0 +1,15 @@
+#-------------------[edit]----------------------------#
+
+from flask import Blueprint
+
+bp = Blueprint('main', __name__, url_prefix='/')
+
+@bp.route('/hello')
+def hello_pybo():
+    return 'Hello, Pybo!'
+
+@bp.route('/')
+def index():
+    return 'Pybo index!'
+
+#-------------------[edit]-----------------------------#
